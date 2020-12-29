887b62f648dad01225e3b08a0693af72662afddb
Miguel <mmellado@mail.sfsu.edu>
2020-12-29 05:44:41 -0800

    actualized

--- secret/index.js
+++ secret/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var favicon = require('serve-favicon');
 
 
 // viewed at http://localhost:8080
 app.use(express.static(__dirname + '/public'));
+
 
 app.listen(3000, () => console.log('Server running on port 3000'))
