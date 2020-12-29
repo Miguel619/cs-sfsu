90b9480f9901d05693cf638168e2f4f822561381
Miguel <mmellado@mail.sfsu.edu>
2020-12-29 02:25:20 -0800

    ssl

--- secret/index.js
+++ secret/index.js
@@ -1,8 +1,33 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 var express = require('express');
+
+const hostname = 'localhost';
+const httpPort = 3000;
+const httpsPort = 443;
+
+const httpsOptions = {
+    cert: fs.readFileSync('./ssl/csatsfsu_xyz.crt'),
+    ca: fs.readFileSync('./ssl/csatsfsu_xyz.ca-bundle'),
+    key: fs.readFileSync('./ssl/cs.key')
+};
+
 var app = express();
 var path = require('path');
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(httpsOptions, app);
+
+app.use((req, res, next) =>{
+    if(req.protocol === 'http') {
+        res.redirect(301, `https://${req.headers.host}${req.url}`);
+      }
+      next();
+});
 
 // viewed at http://localhost:8080
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+httpServer.listen(httpPort, hostname);
+httpsServer.listen(httpsPort, hostname)
